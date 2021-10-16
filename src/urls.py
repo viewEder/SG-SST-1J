981ce0769d356bffb43bfb07876b976b8ed32302
anddyagudelo <anddyagudelo@outlook.com>
2021-10-16 16:36:25 -0500

    actualizacion login

--- src/urls.py
+++ src/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
+    #urls de registration
+    path('accounts/',include('django.contrib.auth.urls'))
 ]
