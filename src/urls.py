29ab290b9e8490962e370b5a923b54693ae53945
anddyagudelo <anddyagudelo@outlook.com>
2021-10-25 18:13:04 -0500

    actualizacion anddy

--- src/urls.py
+++ src/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path, include
@@ -24,3 +25,8 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('registration.urls')),
 ]
+
+# Mostrar imagenes en modo DEBUG:
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
