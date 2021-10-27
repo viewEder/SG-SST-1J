cfd2a9d58c8e5775dbc34308153edafe1d45d52f
anddyagudelo <anddyagudelo@outlook.com>
2021-10-27 18:06:20 -0500

    Finalizacion login

--- registration/models.py
+++ registration/models.py
@@ -26,7 +26,7 @@ class Profile(models.Model):
     profesion = models.CharField(max_length=255,verbose_name="Profesion", null=True, blank=True)
     cedula = models.CharField(max_length=20,verbose_name="Cedula", null=True, blank=True)
     genero = models.CharField(max_length=20, choices=Generos,verbose_name="Genero",default="Otro")
-    fecha_nacimiento = models.DateField(verbose_name="Fecha Nacimiento", null=True,blank=True)
+    fecha_nacimiento = models.DateField(verbose_name="Fecha Nacimiento", null=False,blank=False)
     estado_civil = models.CharField(max_length=20, choices=EstaCivil,verbose_name="Estado Civil",default="No indica")
     
     class Meta:
