8bdb128996c7bbce08b6d5d91a2fcf3d56db1065
camilo <camand5@gmail.com>
2021-10-27 18:48:27 -0500

    configuracion create y modify en las clases

--- empresa/models.py
+++ empresa/models.py
@@ -20,8 +20,8 @@ class Empresa(models.Model): ## Clase que se usara para la creación de las empr
     telefonos = models.CharField(verbose_name='Teléfonos de contacto', max_length=40)
     correo = models.EmailField(verbose_name='Correo electrónico', max_length=255, null=False, blank=False)
     tipo = models.CharField(verbose_name='Tipo de empresa', max_length=100, null=False, blank=False)
-     
-    
+    create_at = models.DateField(auto_now_add=True, verbose_name="Creado el", null=True)
+    modify_at = models.DateField(auto_now=True, verbose_name="Actualizado el")
 
     class Meta:
         verbose_name = 'Empresa'
@@ -33,9 +33,9 @@ class Empresa(models.Model): ## Clase que se usara para la creación de las empr
 class Areas(models.Model):## Clase que se usara para la creación de las áreas que tiene la empresa ingresada
     nombre_area = models.CharField(verbose_name='Nombre Area', max_length= 255, null=False, blank=False)
     nit_empresa = models.ForeignKey(Empresa, on_delete= CASCADE)
+    create_at = models.DateField(auto_now_add=True, verbose_name="Creado el", null=True)
+    modify_at = models.DateField(auto_now=True, verbose_name="Actualizado el")
      
-    
-
     class Meta:
         verbose_name = 'Nombre Area'
         verbose_name_plural = 'Areas de la empresa'
@@ -43,11 +43,12 @@ class Areas(models.Model):## Clase que se usara para la creación de las áreas 
     def __str__(self):
         return f'Nombre Area {self.nombre_area}'
 
+
 class NivelAcademico(models.Model):## Clase que se usara para la creación de los nieveles académicos o estudios de los empleados
     nivel = models.CharField(verbose_name='Nivel Académico', max_length= 255, null=False)
+    create_at = models.DateField(auto_now_add=True, verbose_name="Creado el", null=True)
+    modify_at = models.DateField(auto_now=True, verbose_name="Actualizado el")
      
-    
-
     class Meta:
         verbose_name = 'Nivel Académico'
         verbose_name_plural = 'Niveles Académicos'
@@ -55,18 +56,20 @@ class NivelAcademico(models.Model):## Clase que se usara para la creación de lo
     def __str__(self):
         return f'Nivel Académico {self.nivel}'
 
+
 class Cargos(models.Model):
     cargo = models.CharField(verbose_name='Cargo', max_length= 255, null=False)
     salario_cargo = models.DecimalField(verbose_name='Salario',max_digits=12, decimal_places=2, default=0)
+    create_at = models.DateField(auto_now_add=True, verbose_name="Creado el", null=True)
+    modify_at = models.DateField(auto_now=True, verbose_name="Actualizado el")
      
-    
-
     class Meta:
         verbose_name = 'Cargo'
         verbose_name_plural = 'Cargos'
 
     def __str__(self):
         return f'Cargo {self.cargo}'
+
 
 class Empleado(models.Model): ## Clase destinadad a la creación de los empleados
     codigo_empleado = models.CharField(verbose_name='Código de Empleado', max_length= 20, null=False)
@@ -79,9 +82,9 @@ class Empleado(models.Model): ## Clase destinadad a la creación de los empleado
     ssp = models.CharField(verbose_name='EPS', max_length= 100, null=False)
     sss = models.CharField(verbose_name='Fondo Pensiones', max_length= 100, null=False)
     cuenta_bancaria =  models.CharField(verbose_name='Número de cuenta', max_length= 20, null=False)
+    create_at = models.DateField(auto_now_add=True, verbose_name="Creado el", null=True)
+    modify_at = models.DateField(auto_now=True, verbose_name="Actualizado el")
      
-    
-
     class Meta:
         verbose_name = 'Empleado'
         verbose_name_plural = 'Empleados'
@@ -92,9 +95,9 @@ class Empleado(models.Model): ## Clase destinadad a la creación de los empleado
 
 class Responsabilidades(models.Model):
     responsabilidad = models.TextField(verbose_name='Responsabilidad', null=True, blank=True)
+    create_at = models.DateField(auto_now_add=True, verbose_name="Creado el", null=True)
+    modify_at = models.DateField(auto_now=True, verbose_name="Actualizado el")
      
-    
-
     class Meta:
         verbose_name = 'Responsabiliad'
         verbose_name_plural = 'Responsabiliades'
