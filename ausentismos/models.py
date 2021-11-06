e9d1678c830fe806c9849966ea29982629f0f292
camilo <camand5@gmail.com>
2021-11-06 11:20:26 -0500

    Agreagdo ausentismos

---
+++ ausentismos/models.py
@@ -0,0 +1,11 @@
+import datetime
+from django.db import models
+from django.db.models.base import Model
+from datetime import date
+from django.db.models.deletion import CASCADE
+
+
+# Create your models here.
+
+class Incapacidades(models.Model):
+    pass
--- empresa/models.py
+++ empresa/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.db.models.base import Model
 from datetime import date
 from django.db.models.deletion import CASCADE
-from django.db.models.fields import CharField
+# from django.db.models.fields import CharField
 from django.dispatch import receiver            # Libreria para hacer los cambios en los datos
 from core.types.pandemia import Espandemia
 from core.types.contrato import Contrato
@@ -93,16 +93,12 @@ class Empleado(models.Model): ## Clase destinadad a la creación de los empleado
     tipo_contrato = models.CharField(verbose_name="Tipo Contrato",max_length=255, choices=Contrato)
     antiguedad = models.DecimalField(verbose_name="Tiempo en la empresa", max_digits=2, decimal_places=2,default=0)
     cuenta_bancaria =  models.CharField(verbose_name='Número de cuenta', max_length= 20, null=False)
-<<<<<<< HEAD
     barrio = models.CharField(verbose_name="Barrio", max_length=255, null=True)
     ciudad = models.CharField(verbose_name="Ciudad", max_length=255, null=False)
     departamento = models.CharField(verbose_name="Departamento", max_length=255, null=False)
     estrato = models.IntegerField(verbose_name="Estrato", null=True, blank=True)
     fecha_retiro = models.DateField(verbose_name="Fecha de Retiro")
     create_at = models.DateField(auto_now_add=True, verbose_name="Creado el", null=True)
-=======
-    create_at = models.DateField(auto_now_add=True, verbose_name="Creado el", null=True) 
->>>>>>> 28e6beb2da9dd7f40174708e5033be282d1a3312
     modify_at = models.DateField(auto_now=True, verbose_name="Actualizado el")
 
 
@@ -132,11 +128,7 @@ class Capacitaciones(models.Model):
 
 class Sanidad(models.Model):
     enfermedad = models.CharField(verbose_name="Tipo de Enfermedad",max_length=255)
-<<<<<<< HEAD
     pandemia = models.CharField(verbose_name="Es Pandemia?",max_length=255, choices=Espandemia)## poner en un choicer
-=======
-    pandemia = models.CharField(verbose_name="Es Pandemia?", max_length=2)
->>>>>>> 28e6beb2da9dd7f40174708e5033be282d1a3312
     create_at = models.DateField(auto_now_add=True, verbose_name="Creado el")
     modify_at = models.DateField(auto_now=True, verbose_name="Actualizado el")    
 
