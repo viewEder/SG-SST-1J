c1a4e58c355606c946bc3daabf8d102df9556c57
Eder Lara Trujillo <ederlatru@gmail.com>
2021-11-20 16:22:14 -0500

    Correcciones Por Hacer

--- accidentabilidad/apps.py
+++ accidentabilidad/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AccidentabilidadConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accidentabilidad'
+    verbose_name = 'Configuración de Accidentes Laborales'
